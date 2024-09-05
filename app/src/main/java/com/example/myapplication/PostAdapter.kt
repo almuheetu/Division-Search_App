027a3d21fa11d5cd5ce004bcad99bb557d630c6d
Softzino <almuheetu@gmail.com>
2024-09-05 15:58:30 +0600

    apost fragment add

---
+++ app/src/main/java/com/example/myapplication/PostAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+
+import com.example.myapplication.databinding.AdapterPostBinding
+
+class PostAdapter(
+    private val postList: ArrayList<Post>,
+
+    ) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+    class ViewHolder(val binding: AdapterPostBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val binding = AdapterPostBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup,
+            false
+
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val post = postList[position]
+        viewHolder.binding.tvPostName.text = post.postName
+
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/PostFragment.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentPostListBinding
+import com.example.myapplication.placeholder.PlaceholderContent
+
+class PostFragment : Fragment() {
+    private lateinit var binding: FragmentPostListBinding
+    private lateinit var postAdapter: PostAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPostListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = binding.postRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        postAdapter = PostAdapter(postList)
+        recyclerView.adapter = postAdapter
+
+    }
+
+}
